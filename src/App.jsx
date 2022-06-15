059696e369f47189513ab21c93ae87175a72f60f
lyzzandro <lyzzandro91@gmail.com>
2022-06-15 10:58:18 -0300

    configuracoes do projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1>Título</h1>
+            <form>
+                <input type="text" placeholder="Cidade"></input>
+            </form>
+        </div>
+    );
+}
+
+export default App;
