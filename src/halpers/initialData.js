2e417135357707b0196272dbd8741965ed3f8801
lyzzandro <lyzzandro91@gmail.com>
2022-06-15 17:45:59 -0300

    corrigindo erros de formatacao

--- src/halpers/initialData.js
+++ src/halpers/initialData.js
@@ -1,16 +1,16 @@
 const initialData = {
-    location: {
-        name: "-",
-        region: "-",
-        country: "-",
+  location: {
+    name: '-',
+    region: '-',
+    country: '-',
+  },
+  current: {
+    temp_c: 0,
+    condition: {
+      text: '-',
+      icon: '//cdn.weatherapi.com/weather/64x64/day/122.png',
     },
-    current: {
-        temp_c: 0,
-        condition: {
-            text: "-",
-            icon: "//cdn.weatherapi.com/weather/64x64/day/122.png",
-        },
-    },
+  },
 };
 
 export default initialData;
--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
-const KEY = "3de4be17c21345b5960185013221506";
+const KEY = '3de4be17c21345b5960185013221506';
 const fetchData = async (city) => {
-    const url = `https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
+  const url = `https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
 
-    const fetchResponse = await fetch(url);
-    const data = await fetchResponse.json();
+  const fetchResponse = await fetch(url);
+  const data = await fetchResponse.json();
 
-    return data;
+  return data;
 };
 
 export default fetchData;
